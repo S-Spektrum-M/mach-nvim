c601a0e8bc9586ce279cc02db770aa4f84ee5404
Siddharth Mohanty <neosiddharth+git@gmail.com>
2025-04-09 03:15:01 -0700

    add: blackbird graphic

--- lua/plugins/ui/dashboard.lua
+++ lua/plugins/ui/dashboard.lua
@@ -1,22 +1,44 @@
-return {
-    "goolord/alpha-nvim",
-    -- dependencies = { 'echasnovski/mini.icons' },
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        local alpha = require("alpha")
-        local theme = require("alpha.themes.dashboard")
-
-        theme.section.header.val = {
-
-            [[╭━╮╭━╮╱╱╭━━━┳╮╱╭╮]],
-            [[┃┃╰╯┃┃╱╱┃╭━╮┃┃╱┃┃]],
-            [[┃╭╮╭╮┣━━┫┃╱╰┫╰━╯┃]],
-            [[┃┃┃┃┃┃╭╮┃┃╱╭┫╭━╮┃]],
-            [[┃┃┃┃┃┃╭╮┃╰━╯┃┃╱┃┃]],
-            [[╰╯╰╯╰┻╯╰┻━━━┻╯╱╰╯]]
-        }
-
-        alpha.setup(theme.config)
-
-    end,
+-- source: https://asciiart.cc/view/11079
+local HEADER = {
+"", "", "", "", "", "", "", "", "", "",
+[[                  .                             .                  ]],
+[[                 //                             \\                 ]],
+[[                //                               \\                ]],
+[[               //                                 \\               ]],
+[[              //                _._                \\              ]],
+[[           .---.              .//|\\.              .---.           ]],
+[[ ________ / .-. \_________..-~ _.-._ ~-..________ / .-. \_________ ]],
+[[          \ ._. /    H-     '--.___.--'     -H    \ ._. /          ]],
+[[            ---      H          [H]          H      ---            ]],
+[[                    _H_         _H_         _H_                    ]],
+[[                    UUU         UUU         UUU                    ]],
+"", "", "", "", "", "", "", "",
 }
+return {
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("dashboard").setup({
+      theme = "doom",
+      config = {
+        header = HEADER,
+        headr_opts = { hl = "DashboardHeader", },
+        center = {
+          { icon = "  ", desc = "New File", action = "enew", key = "n" },
+          { icon = "  ", desc = "Find File", action = "Telescope find_files", key = "p" },
+          { icon = "  ", desc = "Recent Files", action = "Telescope oldfiles", key = "r" },
+          { icon = "󰒲  ", desc = "Update", action = "Update", key = "u" },
+          { icon = "  ", desc = "Quit", action = "qa", key = "q" },
+        },
+        footer = {
+          "",
+          "",
+          "",
+          "https://github.com/S-Spektrum-M/mach-nvim"
+        },
+      },
+    })
+  end,
+}
+
