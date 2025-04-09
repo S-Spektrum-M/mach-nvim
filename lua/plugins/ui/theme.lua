ca54113c2308473184a048daa09e2437a646fec7
Siddharth Mohanty <neosiddharth+git@gmail.com>
2025-04-09 02:05:47 -0700

    add theme config

--- lua/plugins/ui/theme.lua
+++ lua/plugins/ui/theme.lua
@@ -3,5 +3,11 @@ return {
         -- "nyoom-engineering/oxocarbon.nvim"
     },
     -- "navarasu/onedark.nvim",
-    { "S-Spektrum-M/odyssey.nvim", event = 'ColorSchemePre' },
+    {
+        "S-Spektrum-M/odyssey.nvim",
+        event = 'ColorSchemePre',
+        config = function()
+            require('odyssey').setup('odyssey')
+        end
+    },
 }
