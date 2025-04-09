423db51d7078ea1b1ece600eb226074793df867c
Siddharth Mohanty <neosiddharth+git@gmail.com>
2025-04-09 12:45:35 -0700

    remove commented part

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,30 +1,3 @@
--- -- type: string
--- -- $HOME/.local/share/nvim
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
---
--- if not vim.loop.fs_stat(lazypath) then
---     vim.fn.system({
---         "git",
---         "clone",
---         "--filter=blob:none",
---         "https://github.com/folke/lazy.nvim.git",
---         "--branch=stable", -- latest stable release
---         lazypath
---     })
--- end
--- vim.opt.rtp:prepend(lazypath)
---
--- require("lazy").setup({
---     require('plugins.files'), -- FILE EXPLORATION
---     require('plugins.editor'), -- EDITOR FEATURES
---     require('plugins.git'),   --  GIT
---     require('plugins.lsp'),   -- LSP
---     require('plugins.ui'),    -- Appearance
---     require('plugins.terminal'),    -- Appearance
--- }, nil)
-
--- type: string
--- $HOME/.local/share/nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
