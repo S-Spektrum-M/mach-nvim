8fccaf2e428135088f9b295d906b1b9fdf288bb5
Siddharth Mohanty <neosiddharth+git@gmail.com>
2025-04-16 12:29:36 -0700

    this file shouldn't be part of the repo

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -6,4 +6,13 @@ return {
     --     lazy = true,
     --     priority = 1000,
     -- }
+    {
+        'lervag/vimtex',
+        tag = "v2.14",
+        config = function()
+            vim.g.vimtex_view_method = 'zathura'
+            vim.g.vimtex_compiler_method = 'latexmk'
+        end,
+        ft = { "tex", "latex", "cls", "tikz" }, -- defer ftdetect
+    }
 }
