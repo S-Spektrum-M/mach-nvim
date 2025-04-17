146542cc97660cc6b2a3828211ea55a27a3b1ee4
Siddharth Mohanty <neosiddharth+git@gmail.com>
2025-04-17 07:13:57 -0700

    This shouldn't exist

--- lua/plugins/custom.lua
+++
@@ -1,9 +0,0 @@
--- this is where you can specify your own plugins
-return {
-    -- example:
-    -- {
-    --     "S-Spektrum-M/odyssey.nvim",
-    --     lazy = true,
-    --     priority = 1000,
-    -- }
-}
