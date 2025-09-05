0039539478e834ac3f60a99bfe3eb4dae52261c4
Siddharth Mohanty <neosiddharth@gmail.com>
2025-09-05 14:55:13 -0700

    feat: blackbird colorscheme

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,12 +1,11 @@
 return {
     {
-        "navarasu/onedark.nvim",
+        "S-Spektrum-M/blackbird.nvim",
         event = "ColorSchemePre",
         lazy = false,
         priority = 1000,
         config = function()
-            require("onedark").setup(vim.mach_opts.onedark)
-            vim.cmd.colorscheme("onedark") -- probably the least jarring theme
+            vim.cmd.colorscheme("blackbird")
         end
     },
 }
