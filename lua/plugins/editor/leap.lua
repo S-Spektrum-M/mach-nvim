1961c4ecd3b02a9f69ba8c2e71c3298e6ee175b3
Siddharth Mohanty <neosiddharth@gmail.com>
2025-09-13 02:29:14 -0700

    removing unused plugins

--- lua/plugins/editor/leap.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'ggandor/leap.nvim',
-    keys = { "s", "S" },       -- or whatever keys you mapped it to
-    config = function()
-        require('leap').add_default_mappings()
-    end,
-}
