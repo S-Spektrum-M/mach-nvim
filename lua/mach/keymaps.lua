cd332c784f3319ab8a6fab184afb29f772cd42f1
Siddharth Mohanty <neosiddharth@gmail.com>
2026-03-09 18:21:48 -0700

    keymaps for cached files

--- lua/mach/keymaps.lua
+++ lua/mach/keymaps.lua
@@ -1,6 +1,17 @@
 local map = vim.keymap.set
-local opts_silent = { noremap = true, silent = true }
 
-map("n", "<Leader>l", "<cmd>nohlsearch<CR>", opts_silent)
-map("v", "x", "d", opts_silent)
-map("n", "U", "<C-r>", opts_silent) -- stolen from helix
+local function silent_opts_desc(desc)
+    return {
+        noremap = true,
+        silent = true,
+        desc = desc
+    }
+end
+
+map("n", "<Leader>l", "<cmd>nohlsearch<CR>", silent_opts_desc("clear search highlight"))
+map("n", "\\", "<cmd>bnext<CR>", silent_opts_desc("go to next buffer"))
+map("n", "|", "<cmd>bprevious<CR>", silent_opts_desc("go to bprevious buffer"))
+map("v", "x", "d", silent_opts_desc("delete selection"))
+map("n", "U", "<C-r>", silent_opts_desc("undo"))
+map("n", "<Leader>if", "<cmd>InitFdCache<CR>", silent_opts_desc("build cache for fd"))
+map("n", "<Leader>ff", "<cmd>ListFdFiles<CR>", silent_opts_desc("list files from fd cache"))
