8178d0c3e15cd6f4ec38be449abbcdc606010fb8
Siddharth Mohanty <neosiddharth@gmail.com>
2026-03-27 06:05:34 -0700

    feature: diffview

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -1,9 +1,26 @@
-return
-{
-    "esmuellert/vscode-diff.nvim",
+return {
+    "esmuellert/codediff.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
     cmd = "CodeDiff",
     keys = {
-        { "<Leader>dt", "<cmd>CodeDiff<CR>",  desc = "Toggle Diff View" },
-    }
+        { "<Leader>dt", "<cmd>CodeDiff<CR>",         desc = "Toggle Git Explorer (CodeDiff)" },
+        { "<Leader>dh", "<cmd>CodeDiff history<CR>", desc = "File History (CodeDiff)" },
+        { "<Leader>dc", "<cmd>CodeDiff HEAD<CR>",    desc = "Compare with HEAD (CodeDiff)" },
+    },
+    opts = {
+        diff = {
+            layout = "side-by-side",
+            compute_moves = true,
+            disable_inlay_hints = true,
+            hide_merge_artifacts = true,
+        },
+        explorer = {
+            view_mode = "tree",
+            flatten_dirs = true,
+            position = "left",
+        },
+        history = {
+            position = "bottom",
+        },
+    },
 }
