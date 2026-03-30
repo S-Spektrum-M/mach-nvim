880ac307fec47c6590f444d166c00b21dd386481
Siddharth Mohanty <neosiddharth@gmail.com>
2026-03-29 22:54:28 -0700

    fix: typos in git keybinds

--- lua/plugins/git/fugitive.lua
+++ lua/plugins/git/fugitive.lua
@@ -6,10 +6,10 @@ return {
         { '<leader>gC', ':Git commit % --amend',    desc = 'ammend current file to commit' },
         { '<leader>ga', '<cmd>Git add %<CR>', desc = 'add current file to commit', },
         { '<leader>gA', '<cmd>Git restore --stage %<CR>', desc = 'remove current file from commit', },
-        { '<leader>gc', ':Git commit %',    desc = 'commit current file' },
+        { '<leader>gc', ':Git commit',    desc = 'commit' },
         { '<leader>gs', git_st,             desc = 'Git status picker' },
         { '<leader>gp', ':Git push origin', desc = 'push commits to origin' },
-        { '<leader>gP', ':Git pull origin', desc = 'push from origin' },
+        { '<leader>gP', ':Git pull origin', desc = 'pull from origin' },
         {
             '<leader>gL',
             '<cmd>Git log --graph --date=relative --pretty=tformat:\'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ad)%Creset\'<CR>',
