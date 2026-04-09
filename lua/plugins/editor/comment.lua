64c35c53e339768a0fcbe01608afc2c00b43b0f8
Siddharth Mohanty <neosiddharth@gmail.com>
2026-04-09 11:53:55 -0700

    removed: comment.nvim

--- lua/plugins/editor/comment.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "numToStr/Comment.nvim",
-    event = { "ModeChanged" },
-    config = function ()
-        require('Comment').setup()
-    end,
-    opts = vim.mach_opts.comment,
-    lazy = true,
-}
