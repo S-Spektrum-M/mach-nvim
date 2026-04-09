cfa8078cd21d1cadccff56630b5f0374e481aad8
Siddharth Mohanty <neosiddharth@gmail.com>
2026-04-09 12:09:56 -0700

    fix: harper lsp
    - Communicate over stdio instead of TCP4000
    - Rename from harper -> harper-ls

--- lsp/harper-ls.lua
+++ lsp/harper-ls.lua
@@ -3,12 +3,8 @@ local function exe_name(lsp_name)
 end
 
 local default_config = {
-    cmd = { exe_name('harper-ls') },
+    cmd = { exe_name('harper-ls'), '--stdio' },
     filetypes = { 'markdown', 'text', 'latex', 'plaintex',  },
-    on_attach = function(client, bufnr)
-        local navic = require("nvim-navic")
-        navic.attach(client, bufnr)
-    end
 }
 
 return default_config
