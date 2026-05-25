6ba3576c5b749ba8cfd560546a4ddf985d3fd9d3
Siddharth Mohanty <neosiddharth@gmail.com>
2026-05-24 17:16:39 -0700

    fix: mark config local

--- lsp/denols.lua
+++ lsp/denols.lua
@@ -2,7 +2,7 @@ local function exe_name(lsp_name)
     return vim.fn.stdpath('data') .. '/mason/bin/' .. lsp_name
 end
 
-default_config = {
+local default_config = {
     cmd = { exe_name('deno'), 'lsp' },
     cmd_env = { NO_COLOR = true },
     filetypes = {
