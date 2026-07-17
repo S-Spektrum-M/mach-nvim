31cfad67c1206062fdd2a158eaabeb1627a2ddd7
Siddharth Mohanty <neosiddharth@gmail.com>
2026-07-17 02:47:13 -0700

    feat: add clangd multi-threading opts

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,10 +1,21 @@
 local exe_name = require('mach.lsp_util').exe_name
 
+local nproc = tonumber (vim.fn.system ({"nproc"}))
+
+-- Credit: https://gasparvardanyan.github.io/blog/tips-for-cpp-developers/
+if 0 ~= nproc
+then
+    local jnproc =  "--j=" .. (nproc - 1)
+end
+
 local default_config = {
     cmd = {
         exe_name('clangd'),
-        '--clang-tidy' ,
-        '--query-driver=/bin/g++'
+        '--clang-tidy',
+        '--query-driver=/bin/g++',
+        '--background-index',
+        jnproc,
+
     },
     filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
     root_markers = { '.git', '.clang-format', 'CMakeLists.txt', 'Makefile' },
