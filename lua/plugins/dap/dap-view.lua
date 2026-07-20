f7691a49622a82bf02206d7380d7d5ac3f4c61bf
Siddharth Mohanty <neosiddharth@gmail.com>
2026-07-20 02:20:35 -0700

    perf: lazy load dap

--- lua/plugins/dap/dap-view.lua
+++ lua/plugins/dap/dap-view.lua
@@ -2,10 +2,17 @@ return {
     {
         "igorlfs/nvim-dap-view",
         -- let the plugin lazy load itself
-        lazy = false,
+        lazy = true,
         version = "1.*",
         ---@module 'dap-view'
         ---@type dapview.Config
         opts = {},
+        cmd = {
+            "DapViewOpen",
+            "DapViewClose",
+            "DapViewToggle",
+            "DapViewHover",
+            "DapViewWatch",
+        },
     },
 }
