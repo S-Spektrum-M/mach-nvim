cfddfd4e27618989b522d9d9f620b98516e3e967
Siddharth Mohanty <neosiddharth@gmail.com>
2026-07-20 01:29:28 -0700

    perf: lazy load lsp

--- lua/mach/init.lua
+++ lua/mach/init.lua
@@ -4,5 +4,12 @@ require("mach.commands")
 require("mach.features")
 require("mach.keymaps")
 require("mach.functions")
-require("mach.lsp")
 require("mach.autocommands")
+
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+    group = vim.api.nvim_create_augroup("MachLspSetup", { clear = true }),
+    once = true,
+    callback = function()
+        require("mach.lsp")
+    end,
+})
