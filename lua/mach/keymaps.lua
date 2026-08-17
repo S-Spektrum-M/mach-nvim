20c0fa26d659774bc0d378264433aa5e5476c648
Siddharth Mohanty <neosiddharth@gmail.com>
2026-08-17 01:43:42 -0700

    al/il keybinds

--- lua/mach/keymaps.lua
+++ lua/mach/keymaps.lua
@@ -12,6 +12,15 @@ map("n", "<Leader>l", "<cmd>nohlsearch<CR>", silent_opts_desc("clear search high
 map("n", "\\", "<cmd>bnext<CR>", silent_opts_desc("go to next buffer"))
 map("n", "|", "<cmd>bprevious<CR>", silent_opts_desc("go to bprevious buffer"))
 map("v", "x", "d", silent_opts_desc("delete selection"))
+
+-- Inner Line (il): first non-blank character to last non-blank character.
+map("x", "il", "g_o^", silent_opts_desc("select inner line"))
+map("o", "il", ":normal vil<CR>", silent_opts_desc("target inner line"))
+
+-- Around Line (al): entire line, including leading and trailing whitespace.
+map("x", "al", "$o0", silent_opts_desc("select around line"))
+map("o", "al", ":normal val<CR>", silent_opts_desc("target around line"))
+
 map("n", "U", "<C-r>", silent_opts_desc("undo"))
 map("n", "<Leader>if", "<cmd>InitFdCache<CR>", silent_opts_desc("build cache for fd"))
 map("n", "<Leader>ff", "<cmd>ListFdFiles<CR>", silent_opts_desc("list files from fd cache"))
